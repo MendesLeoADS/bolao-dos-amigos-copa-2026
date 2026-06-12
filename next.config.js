0c995a08ad44d0f332d466b736d55439e8b306fa
MendesLeoADS <devleonardo.tech@gmail.com>
2026-06-12 20:56:40 -0300

    att app 2

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'media.api-sports.io',
       },
+      {
+        protocol: 'https',
+        hostname: 'crests.football-data.org',
+      },
     ],
   },
 };
